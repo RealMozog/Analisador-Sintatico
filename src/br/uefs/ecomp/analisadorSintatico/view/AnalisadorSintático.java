5d94f4b23e9a4340cb1a87755235971375971ef2
Alessandro Costa <mozoguito@gmail.com>
2020-02-17 20:44:57 -0300

    Pastas do projeto

---
+++ src/br/uefs/ecomp/analisadorSintatico/view/AnalisadorSintático.java
@@ -0,0 +1,17 @@
+package br.uefs.ecomp.analisadorSintatico.view;
+
+/**
+ *
+ * @author Alessandro Costa
+ */
+public class AnalisadorSintático {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception {
+        
+    }
+    
+}
